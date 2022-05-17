afd3a3337f0f8a78e8327349aaa9308f2e1c4fe8
EvanHendersonRichtByte <adamhenderson3x3@gmail.com>
2022-05-17 11:11:05 +0700

    Add mysql dependency

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,19 @@
 package main;
+import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
+    static Scanner in = new Scanner(System.in);
+    static Connection conn = null;
+    static Statement st = null;
+    static ResultSet rs = null;
+
     public static void main(String[] args) {
-        
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/pplbo_quiz2", "root", "");
+            st = conn.createStatement();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
